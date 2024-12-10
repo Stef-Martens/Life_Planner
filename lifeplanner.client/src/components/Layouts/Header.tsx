fafca68c2a7edea6785806560e772a4fe5acbc84
stef <stef.m@hotmail.be>
2024-12-11 00:21:53 +0100

    some changes to layout of dashboard

--- lifeplanner.client/src/components/Layouts/Header.tsx
+++ lifeplanner.client/src/components/Layouts/Header.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import LogoutButton from "../Buttons/LogoutButton";
 import logo from "../../../public/appa.png";
 import { useAuth0 } from "@auth0/auth0-react";
+import { useLocation } from "react-router-dom"; // Import useLocation
 
 const Header: React.FC = () => {
   const { user } = useAuth0();
+  const location = useLocation(); // Get the current path
 
   // Navigation items can be stored in an array for better scalability
   const navLinks = [
@@ -26,18 +28,22 @@ const Header: React.FC = () => {
 
           {/* Navigation Bar */}
           <nav className="flex flex-wrap w-auto rounded-full h-auto bg-neutral text-neutral-content justify-between">
-            {navLinks.map((link) => (
-              <a
-                key={link.href}
-                href={link.href}
-                className={
-                  "hover:bg-base-300 rounded-full px-6 sm:px-3 md:px-2 lg:px-6 xl:px-8 py-3  transition-colors duration-300"
-                }
-                aria-label={link.label}
-              >
-                {link.label}
-              </a>
-            ))}
+            {navLinks.map((link) => {
+              const isActive = location.pathname === link.href; // Check if the link is active
+              return (
+                <a
+                  key={link.href}
+                  href={link.href}
+                  className={`
+                    ${isActive ? "text-primary-content font-bold" : ""} 
+                    hover:bg-base-300 rounded-full px-6 sm:px-3 md:px-2 lg:px-6 xl:px-8 py-3 transition-colors duration-300
+                  `}
+                  aria-label={link.label}
+                >
+                  {link.label}
+                </a>
+              );
+            })}
           </nav>
         </div>
 
@@ -46,11 +52,11 @@ const Header: React.FC = () => {
           <button className="btn hover:bg-base-300 border-0 bg-neutral  rounded-full transition-colors duration-300">
             <svg
               xmlns="http://www.w3.org/2000/svg"
+              className="stroke-neutral-content"
               width="24"
               height="24"
               viewBox="0 0 24 24"
               fill="none"
-              stroke="currentColor"
               strokeWidth="2"
             >
               <circle cx="11" cy="11" r="8" />
